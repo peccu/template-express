0f1ae3565bf88deb6e0bf4f8e403d569ff626bdc
peccu <peccul@gmail.com>
2016-04-05 19:05:21 +0900

    add form and post receiver

--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,17 @@ router.get('/', function(req, res){
   res.render('index', response);
 });
 
+router.post('/', function(req, res){
+  console.log(req.body);
+  response.data = req.body;
+  response.datastr = JSON.stringify(req.body);
+
+  if(!req.body || !req.body.message){
+    response.error = {msg: 'message is not specified'};
+    res.render('index', response);
+  }
+
+  res.render('index', response);
+});
+
 module.exports = router;
